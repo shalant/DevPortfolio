7ea605ffa6c6b7c8297adaeca2608aab3e61cbc8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-28 20:52:05 -0600

    working on basic frontend

--- Shared/Models/Category.cs
+++ Shared/Models/Category.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Models
 {
-    internal class Category
+    public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string ThumbnailImagePath { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
     }
 }
